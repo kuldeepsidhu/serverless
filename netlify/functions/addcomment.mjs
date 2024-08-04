82342d3e848e373fd3ccd9863e361ba768985cda
kuldeepsidhu <kuldeepsidhu007@gmail.com>
2024-08-04 19:31:26 +0530

    Update addcomment.mjs

--- netlify/functions/addcomment.mjs
+++ netlify/functions/addcomment.mjs
@@ -1,27 +1,9 @@
 export default async (req, context) => {
-  try {
-    // Fetch data from the API
-    const data = {};
-    data.url = req.url;
-    data.method = req.method;
-    data.body = req.body;
-    data.bodyUsed = req.bodyUsed;
-    data.context = context;
+  const { city, country } = context.params;
 
-    // Parse the request body
-    // const requestBody = JSON.parse(req);
-    // data.parsedBody = requestBody;
+  return new Response(`You're visiting ${city} in ${country}!`);
+};
 
-    
-    return new Response(JSON.stringify(data), {
-      headers: { 'Content-Type': 'application/json' }
-    });
-  } catch (error) {
-    // Handle any errors that occurred during the fetch
-    console.error('Error fetching data:', error);
-    return new Response(JSON.stringify({ error: error.message }), {
-      status: 500,
-      headers: { 'Content-Type': 'application/json' }
-    });
-  }
+export const config = {
+  path: "/travel-guide/:city/:country"
 };

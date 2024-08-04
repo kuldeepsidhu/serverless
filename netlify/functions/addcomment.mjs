43218fbb15b603e0159a95b416aa55bf1b1861f5
kuldeepsidhu <kuldeepsidhu007@gmail.com>
2024-08-04 19:07:24 +0530

    Update addcomment.mjs

--- netlify/functions/addcomment.mjs
+++ netlify/functions/addcomment.mjs
@@ -1,10 +1,13 @@
 export default async (req, context) => {
   try {
     // Fetch data from the API
-    const response = {};
-    response.request = req;
-    response.context = context;
-    return new Response(JSON.stringify(response), {
+    const data = {};
+    data.request.url = req.url;
+    data.request.method = req.method;
+    data.request.body = req.body;
+    data.request.bodyUsed = req.bodyUsed;
+    data.context = context;
+    return new Response(JSON.stringify(data), {
       headers: { 'Content-Type': 'application/json' }
     });
   } catch (error) {
